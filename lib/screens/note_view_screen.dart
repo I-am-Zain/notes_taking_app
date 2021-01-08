17e5c9519d62cbd1dc297763b0076553c1fd54f4
Zain ul Abideen <58477980+I-am-Zain@users.noreply.github.com>
2021-01-08 18:35:05 +0500

    noteView updated with alert on delete addded ✔

--- lib/screens/note_view_screen.dart
+++ lib/screens/note_view_screen.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:notes_taking_app/models/note.dart';
+import 'package:notes_taking_app/utils/constants.dart';
+
+import 'note_edit_screen.dart';
 class NoteViewScreen extends StatefulWidget {
   static const route='/note-view';
   @override
@@ -6,8 +12,79 @@ class NoteViewScreen extends StatefulWidget {
 }
 
 class _NoteViewScreenState extends State<NoteViewScreen> {
+  Note selectedNote;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: white,
+      appBar: AppBar(
+        elevation: 0.7,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: [
+
+          IconButton(
+            icon: Icon(
+              Icons.delete,
+              color: Colors.black,
+            ),
+            onPressed: () => _showDialog(),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                selectedNote?.title,
+                style: viewTitleStyle,
+              ),
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.access_time,
+                    size: 18,
+                  ),
+                ),
+                Text('${selectedNote?.date}')
+              ],
+            ),
+            if (selectedNote.imagePath != null)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Image.file(File(selectedNote.imagePath)),
+              ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                selectedNote.content,
+                style: viewContentStyle,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, NoteEditScreen.route,
+              arguments: selectedNote.id);
+        },
+        child: Icon(Icons.edit),
+      ),
+    );
   }
 }
