4b72af888502ad8fd44a299fa7cff9876dc56a72
Zain ul Abideen <58477980+I-am-Zain@users.noreply.github.com>
2021-01-08 19:42:23 +0500

    Dismissable (Swipe to Delete Note) DOne ✔

--- lib/screens/note_list_screen.dart
+++ lib/screens/note_list_screen.dart
@@ -45,12 +45,24 @@ class _NoteListScreenState extends State<NoteListScreen> {
                       {
                         final i = index - 1;
                         final item = noteprovider.items[i];
-                        return ListItem(
-                          id: item.id,
-                          title: item.title,
-                          content: item.content,
-                          imagePath: item.imagePath,
-                          date: item.date,
+                        return Dismissible(key: Key('$item'),
+                          onDismissed: (direction){
+                            setState(() {
+                              Provider.of<NoteProvider>(context, listen: false)
+                                  .deleteNote(item.id);
+                              // item.removeAt(i);
+                            });
+                            Scaffold.of(context)
+                                .showSnackBar(SnackBar(content: Text("Deleted Successfully ")));
+                          },
+                          background: Container(color: Colors.red),
+                          child: ListItem(
+                            id: item.id,
+                            title: item.title,
+                            content: item.content,
+                            imagePath: item.imagePath,
+                            date: item.date,
+                          ),
                         );
                       }
                     },
