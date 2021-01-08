c1e5c4cb4c6faaf5ed47e0c207aa888f3fc4d113
Zain ul Abideen <58477980+I-am-Zain@users.noreply.github.com>
2021-01-08 17:26:26 +0500

    created packages with related dart files

---
+++ lib/screens/note_list_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class NoteListScreen extends StatefulWidget {
+  @override
+  _NoteListScreenState createState() => _NoteListScreenState();
+}
+
+class _NoteListScreenState extends State<NoteListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
